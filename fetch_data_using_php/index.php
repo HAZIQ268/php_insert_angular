fd47d8a51528195ffb674a58fc8a4fb4c23c02c1
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-08-09 18:52:36 +0500

    done

--- fetch_data_using_php/index.php
+++ fetch_data_using_php/index.php
@@ -1,15 +1,17 @@
 <?php
-header('Access-Control-Allow-Origin :*');
+header('Access-Control-Allow-Origin : *');
 
 $data = json_decode(file_get_contents("php://input"),true);
 
 $connection = mysqli_connect("localhost", "root", "", "php_angular");
 
 
-if ($_SERVER('REQUEST_METHOD') == 'post') {
-    
+if ($_SERVER('REQUEST_METHOD') == "POST") {
+    $name = $data['username'];
+    $name = $data['useremail'];
+    $name = $data['userpass'];
 }
 
 
-$query = mysqli_query($connection, "insert into student (name,email,password) values ");
+$query = mysqli_query($connection, "insert into student (name,email,password) values ('$name','$email','$pass')");
 ?>
--- workkk/php-angular/index.php
+++ workkk/php-angular/index.php
@@ -1,12 +1,12 @@
-<?php
+<!-- <?php
 
-$connection = mysqli_connect("localhost","root","","php_angular");
+// $connection = mysqli_connect("localhost","root","","php_angular");
 
-$query =mysqli_query($connection,"select * from student");
+// $query =mysqli_query($connection,"select * from student");
 
-while($data = mysqli_fetch_assoc($query)){
-    $users[]=$data;
-}
-echo json_encode($users);
+// while($data = mysqli_fetch_assoc($query)){
+//     $users[]=$data;
+// }
+// echo json_encode($users);
 
-?>
+?> -->
